c9aeee30efa895d2e851e01e09f3adb42e2b20b0
Pomazan Vladislav <pomazanvi@gmail.com>
2021-04-30 15:39:04 +0300

    basic auth

--- src/server.js
+++ src/server.js
@@ -1,24 +1,56 @@
 import morgan from 'morgan';
 import helmet from 'helmet';
 import express from 'express';
-import cookieParser from 'cookie-parser';
+import session from 'express-session';
 import routes from './routes/index.js';
+import {isDevelopment, isProduction} from './utils/env.js';
+import SessionStore from '@quixo3/prisma-session-store';
+import prisma from './config/database.js';
+import passport from 'passport';
+import authentication from './services/Auth.js';
 
+const sessionOptions = {
+	secret: process.env.APP_KEY,
+	cookie: {
+	  maxAge: 7 * 24 * 60 * 60 * 1000
+	},
+	resave: true,
+	saveUninitialized: true,
+  	store: new SessionStore.PrismaSessionStore(
+	  prisma,
+	  {
+			checkPeriod: false,
+			dbRecordIdIsSessionId: true,
+			dbRecordIdFunction: undefined,
+	  }
+	),
+};
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
-if (process.env.NODE_ENV === 'development') {
+if (isDevelopment()) {
 	app.use(morgan('dev'));
 }
 
-if (process.env.NODE_ENV === 'production') {
+if (isProduction()) {
+	app.set('trust proxy', 1);
+	sessionOptions.cookie.secure = true;
 	app.use(helmet());
 }
+app.use(session(sessionOptions));
+app.use(passport.initialize());
+app.use(passport.session());
 
 routes(app);
+authentication(passport);
+
+app.post('/api/login',
+	passport.authenticate('local', { successRedirect: '/api/users',
+		failureRedirect: '/login',
+		failureFlash: true })
+);
 
 app.use((err, req, res) => {
 	console.error(err.message);
--- src/utils/env.js
+++ src/utils/env.js
@@ -1,9 +1,15 @@
 const DEVELOPMENT = 'development'; 
+const PRODUCTION = 'production';
 
 export function isDevelopment() {
 	return process.env.NODE_ENV === DEVELOPMENT;
-} 
+}
+
+export function isProduction() {
+  	return process.env.NODE_ENV === DEVELOPMENT;
+}
 
 export const ENV = {
 	DEVELOPMENT: DEVELOPMENT,
+  	PRODUCTION: PRODUCTION,
 };
