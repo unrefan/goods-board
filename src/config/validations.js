f5a275ede062d861bd0c857d65697c39a629cf2a
Pomazan Vladislav <pomazanvi@gmail.com>
2021-05-04 11:44:30 +0300

    ignore unique while updating self

--- src/config/validations.js
+++ src/config/validations.js
@@ -3,16 +3,18 @@ import BaseRepository from '../repositories/base.js';
 
 Validator.registerAsync('unique', async function (value, requirement, attribute, passes) {
   try {
-    const [model, key] = this.ruleValue.split(',');
+    const [model, key, ignoreKey, ignoreValue] = this.ruleValue.split(',');
+    const not = ignoreKey && ignoreValue ? {NOT: {[ignoreKey]: Number(ignoreValue),}} : {};
     const resource = await BaseRepository(model).getClient().findFirst({where: {
       [key]: this.inputValue,
+      ...not
     }, select: {
       	[key]: true,
 	  }
     });
     if (resource) {
       return passes(false, `The given ${key} should be unique`);
-	}
+    }
     return passes();
   } catch (e) {
     passes(false, e.message);
--- src/requests/users.js
+++ src/requests/users.js
@@ -7,10 +7,10 @@ export const create = validation({
   password: ['required', 'string', 'min:6', 'max:255'],
 });
 
-export const updateCurrent = validation({
+export const updateCurrent = (req, res, next) => validation({
   phone: ['string', 'between:10,13', 'regex:/^\\+380[0-9]{9,13}/'],
   name: ['string', 'min:3', 'max:255'],
-  email: ['string', 'email', 'max:255'],
+  email: ['string', 'email', 'max:255', `unique:user,email,id,${req.auth.id}`],
   currentPassword: ['string', 'min:6', 'max:255'],
   newPassword: ['required_with:currentPassword', 'same:currentPassword', 'string', 'min:6', 'max:255'],
-});
+})(req, res, next);
